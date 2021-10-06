297baed4448be7cbb073501001861286f7bb4d55
luizapuccinelli <luizapuccinelli06@gmail.com>
2021-10-06 18:46:55 -0300

    finish org team

---
+++ src/components/Team/Org/Ministerio.js
@@ -0,0 +1,50 @@
+const member = [
+    {
+        name: 'Pietro Braga Figueiredo',
+        role: 'Assessor',
+        course: 'Economia (UFSC)',
+        imageUrl:
+            '/images/membros/ministerio/pietro.png',
+    },
+    {
+        name: 'João Pedro Dalla Valle',
+        role: 'Assessor',
+        course: 'Direito (UFSC)',
+        imageUrl:
+            '/images/membros/ministerio/joao.png',
+    },
+    {
+        name: 'Laísa Ágathe Dacroce',
+        role: 'Assessora',
+        course: 'Relações Internacionais (UFSC)',
+        imageUrl:
+            '/images/membros/ministerio/laisa.png',
+    },
+  ]
+
+function Ministerio(){
+ return (
+    <div>
+        <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+            {member.map((eisteiniano) => (
+            <li key={eisteiniano.name} className="h-96 w-72">
+                <div className="pt-5 space-y-4 bg-white rounded-md drop-shadow-xl h-96 flex flex-col items-center">
+                    <div>
+                        <img className="object-cover shadow-lg rounded-lg h-64" src={eisteiniano.imageUrl} alt="" />
+                    </div>
+
+                    <div className="space-y-1 xl:flex xl:items-center xl:justify-between">
+                            <div className="font-medium text-sm text-center">
+                            <h3 className="text-xl">{eisteiniano.name}</h3>
+                                <p className="text-blue-900 text-center">{eisteiniano.role}</p>
+                                <p className="text-center">{eisteiniano.course}</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            ))}
+        </ul>
+    </div>
+ )
+}
+export default Ministerio
---
+++ src/components/Team/Org/Vale.js
@@ -0,0 +1,50 @@
+const member = [
+    {
+        name: 'Murillo Stein',
+        role: 'Diretor do Vale do Silício',
+        course: 'Engenharia Mecânica (UFSC)',
+        imageUrl:
+            '/images/membros/vale/murillo.png',
+    },
+    {
+        name: 'Djonys Dalmy de Oliveira',
+        role: 'Assessor',
+        course: 'Sistemas da Informação (UFSC)',
+        imageUrl:
+            '/images/membros/vale/djonys.png',
+    },
+    {
+        name: 'Luiza Kataoka P. de Oliveira',
+        role: 'Assessora',
+        course: 'Engenharia de Materiais (UFSC)',
+        imageUrl:
+            '/images/membros/vale/luiza.png',
+    },
+  ]
+
+function Vale(){
+ return (
+    <div>
+        <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+            {member.map((eisteiniano) => (
+            <li key={eisteiniano.name} className="h-96 w-72">
+                <div className="pt-5 space-y-4 bg-white rounded-md drop-shadow-xl h-96 flex flex-col items-center">
+                    <div>
+                        <img className="object-cover shadow-lg rounded-lg h-64" src={eisteiniano.imageUrl} alt="" />
+                    </div>
+
+                    <div className="space-y-1 xl:flex xl:items-center xl:justify-between">
+                            <div className="font-medium text-sm text-center">
+                            <h3 className="text-xl">{eisteiniano.name}</h3>
+                                <p className="text-blue-900 text-center">{eisteiniano.role}</p>
+                                <p className="text-center">{eisteiniano.course}</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            ))}
+        </ul>
+    </div>
+ )
+}
+export default Vale
