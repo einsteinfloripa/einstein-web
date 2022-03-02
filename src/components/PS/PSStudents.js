2365d5fb1888510cb634adb494427683aa3f6e26
luizakataoka <luiza.kataoka@bixtecnologia.com.br>
2022-03-02 14:25:19 -0300

    gabarito

--- src/components/PS/PSStudents.js
+++ src/components/PS/PSStudents.js
@@ -90,12 +90,12 @@ function PSStudents () {
           <div className="relative px-4  sm:px-6 lg:px-8 pb-10">
             <h1 className="text-blue lg:text-4xl text-2xl font-semibold tracking-tight">Links úteis</h1>
             <div className="mt-6 flex justify-start flex-col">
-              <div>
+              <div className="mt-8">
                 <a
-                  href="https://app.pipefy.com/public/form/FxNEJ30S" target="Starfall"
+                  href="https://drive.google.com/file/d/1jNz0MVV6dLwm8DtHupULmXdIWc_16XjP/view?usp=sharing" target="Starfall"
                   className="text-center text-2xl font-semibold inline-flex bg-blue-50 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-blue-900 hover:bg-blue hover:text-white"
                 >
-                  Inscrição para o processo seletivo de alunos de 2022
+                  Gabarito Oficial Prova 2022
                 </a>
               </div>
               <div className="mt-8">
