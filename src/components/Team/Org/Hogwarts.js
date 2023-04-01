92779ee00b7b7daa42dffd63f158bb3f10d9fa7f
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2023-04-01 16:43:59 -0300

    chore: add 'Malau' to members list

--- src/components/Team/Org/Hogwarts.js
+++ src/components/Team/Org/Hogwarts.js
@@ -44,12 +44,26 @@ const member = [
         : "https://einsteinfloripa.com.br/images/org/bruna-franchini.png";
     },
   },
+  {
+    name: "Maria Laura Silva Cabral",
+    role: "Assessora",
+    course: "Farmácia (UFSC)",
+    birthday: "16/01",
+    get isBirthday() {
+      return day === this.birthday;
+    },
+    get imageUrl() {
+      return this.isBirthday
+        ? "https://einsteinfloripa.com.br/images/org/malau.png"
+        : "https://einsteinfloripa.com.br/images/org/malau.png";
+    },
+  },
 ];
 
 function Hogwarts() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p className="text-justify pb-20 w-9/12">
+    <div className='flex flex-col justify-center items-center'>
+      <p className='text-justify pb-20 w-9/12'>
         A qualidade de ensino que o Einstein proporciona é fruto do trabalho dos
         Bruxos que trabalham em Hogwarts, responsáveis desde a organização do
         calendário anual, atividades extraclasse de ensino, dinâmica de aulas,
@@ -58,41 +72,42 @@ function Hogwarts() {
         em melhorias para que possamos proporcionar a melhor experiência
         possível para os alunos!
       </p>
-      <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+      <ul className='space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8'>
         {member.map((eisteiniano) => (
-          <li key={eisteiniano.name} className="h-96 w-72 relative">
+          <li key={eisteiniano.name} className='h-96 w-72 relative'>
             {eisteiniano.isBirthday ? (
-              <div className="conteiner-cake">
-                <div className="cake">
-                  <div className="candle">
-                    <div className="fire"></div>
-                    <div className="fire"></div>
-                    <div className="fire"></div>
-                    <div className="fire"></div>
-                    <div className="fire"></div>
+              <div className='conteiner-cake'>
+                <div className='cake'>
+                  <div className='candle'>
+                    <div className='fire'></div>
+                    <div className='fire'></div>
+                    <div className='fire'></div>
+                    <div className='fire'></div>
+                    <div className='fire'></div>
                   </div>
-                  <div className="frosting"></div>
-                  <div className="bizcocho"></div>
+                  <div className='frosting'></div>
+                  <div className='bizcocho'></div>
                 </div>
               </div>
             ) : (
               <></>
             )}
-            <div className="pt-5 space-y-4 bg-white rounded-md drop-shadow-xl h-96 flex flex-col items-center">
+            <div className='pt-5 space-y-4 bg-white rounded-md drop-shadow-xl h-96 flex flex-col items-center'>
               <div>
                 <img
-                  className={`object-cover shadow-lg rounded-lg h-64 w-64 ${eisteiniano.isBirthday ? "" : "grayscale"
-                    }`}
+                  className={`object-cover shadow-lg rounded-lg h-64 w-64 ${
+                    eisteiniano.isBirthday ? "" : "grayscale"
+                  }`}
                   src={eisteiniano.imageUrl}
-                  alt=""
+                  alt=''
                 />
               </div>
 
-              <div className="space-y-1 xl:flex xl:items-center xl:justify-between">
-                <div className="font-medium text-sm text-center">
-                  <h3 className="text-xl">{eisteiniano.name}</h3>
-                  <p className="text-blue text-center">{eisteiniano.role}</p>
-                  <p className="text-center">{eisteiniano.course}</p>
+              <div className='space-y-1 xl:flex xl:items-center xl:justify-between'>
+                <div className='font-medium text-sm text-center'>
+                  <h3 className='text-xl'>{eisteiniano.name}</h3>
+                  <p className='text-blue text-center'>{eisteiniano.role}</p>
+                  <p className='text-center'>{eisteiniano.course}</p>
                 </div>
               </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,32 @@ import HomeText from "../components/Home/HomeText";
 import HomeNumbers from "../components/Home/HomeNumbers";
 import HomeDonate from "../components/Home/HomeDonate";
 import HomePartners from "../components/Home/HomePartners";
-import BannerBithday from '../components/Banner'
+import BannerBithday from "../components/Banner";
 import member from "../components/Banner/dadosBith";
 
 export default function Home() {
-	const { isBirthday, setIsBirthday, setBirthPerson } = useContext(BirthdayContext);
+  const { isBirthday, setIsBirthday, setBirthPerson } =
+    useContext(BirthdayContext);
 
-	member.forEach((eisteiniano) => {
-		if (eisteiniano.isBirthday) {
-			const dividirNome = eisteiniano.name.split(" ");
-			setIsBirthday(true);
-			setBirthPerson(dividirNome[0]);
-		}
-	});
-	return (
-		<div className="overflow-x-hidden">
-			<Head>
-				<title>Einstein Floripa</title>
-			</Head>
-			{isBirthday ? <BannerBithday /> : <></>}
-			<HomeBanner />
-			<HomeText />
-			<HomeNumbers />
-			<HomeDonate />
-			<HomePartners />
-		</div>
-	);
+  // Funcionalidade de aniversário suspensa por enquanto
+  // member.forEach((eisteiniano) => {
+  // 	if (eisteiniano.isBirthday) {
+  // 		const dividirNome = eisteiniano.name.split(" ");
+  // 		setIsBirthday(true);
+  // 		setBirthPerson(dividirNome[0]);
+  // 	}
+  // });
+  return (
+    <div className='overflow-x-hidden'>
+      <Head>
+        <title>Einstein Floripa</title>
+      </Head>
+      {isBirthday ? <BannerBithday /> : <></>}
+      <HomeBanner />
+      <HomeText />
+      <HomeNumbers />
+      <HomeDonate />
+      <HomePartners />
+    </div>
+  );
 }
