aef64e6f2d2827325d5706fb8f11d48daae26f65
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2023-07-22 16:43:36 -0300

    add 'O Jogo' page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,30 +5,32 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import BirthdayContext from "../components/Contexts/BithdayContext";
+import "normalize.css";
 import "tailwindcss/tailwind.css";
 import "../styles/cake.css";
 import "../styles/fireworks.css";
 
 function MyApp({ Component, pageProps }) {
-	const [isBirthday, setIsBirthday] = useState(false);
-	const [birthPerson, setBirthPerson] = useState(null);
-	return (
-		<BirthdayContext.Provider
-			value={{ isBirthday, setIsBirthday, birthPerson, setBirthPerson }}>
-			<Head>
-				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-				<link rel='preconnect' href='https://fonts.googleapis.com' />
-				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
-					rel='stylesheet'
-				/>
-			</Head>
-			<Navbar />
-			<Component {...pageProps} />
-			<Footer />
-		</BirthdayContext.Provider>
-	);
+  const [isBirthday, setIsBirthday] = useState(false);
+  const [birthPerson, setBirthPerson] = useState(null);
+  return (
+    <BirthdayContext.Provider
+      value={{ isBirthday, setIsBirthday, birthPerson, setBirthPerson }}
+    >
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </BirthdayContext.Provider>
+  );
 }
 
 export default MyApp;
