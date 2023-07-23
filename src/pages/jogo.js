ce08d4896a2eb2948f79a07be5117a7004513cf9
Marcelo Antonio Ecco <92338508+maecco@users.noreply.github.com>
2023-07-23 08:26:45 -0300

    add 'O Jogo' page (#11)

---
+++ src/pages/jogo.js
@@ -0,0 +1,9 @@
+import Form from "src/components/Form/Form";
+
+export default function Jogo() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center bg-jogo bg-cover bg-center'>
+      <Form />
+    </div>
+  );
+}
