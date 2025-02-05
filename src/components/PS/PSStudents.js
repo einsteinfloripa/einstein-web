edb2331c7846f7137512a450eca691ac712f9764
devpedrorocha <pedro.rocha2609@gmail.com>
2025-02-05 17:24:04 -0300

    ajustando link do manual de alunos

--- src/components/PS/PSStudents.js
+++ src/components/PS/PSStudents.js
@@ -167,7 +167,7 @@ function PSStudents() {
             <div className='flex flex-col justify-start mt-6'>
               <div >
                 <a
-                  href='https://drive.google.com/file/d/1kwiwvkYhSHqXp9qk9hhn_nOINNPkcjcR/view?usp=sharing'
+                  href='https://drive.google.com/file/d/1KtllCzF13LhPD-rK3l_SHR5OdwcfXgBz/view?usp=sharing'
                   target='Starfall'
                   className='inline-flex px-4 py-2 text-2xl font-semibold text-center text-blue-900 border border-transparent rounded-md shadow-sm bg-blue-50 bg-origin-border hover:bg-blue hover:text-white'
                 >
